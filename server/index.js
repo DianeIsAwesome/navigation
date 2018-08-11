764265e2eeea91840b50c4470e61d3510e25e49c
Chris W <christopherkwildenradt@gmail.com>
2018-08-11 08:49:46 -0700

    Update all routes to work with postgres

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const parser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 const model = require('./model');
 
 const port = process.env.PORT || 2999;
@@ -12,16 +12,17 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(cors());
 
-// app.get('/api/searchRecords', (req, res) => {
-//   model.getSearchRecords((err, results) => {
-//     if (err) console.log(err);
-//     res.statusCode = err ? 400 : 200;
-//     res.send(err || results);
-//   });
-// });
+app.get('/api/searchRecords', (req, res) => {
+  console.log('get search records called to server')
+  model.getSearchRecords((err, results) => {
+    if (err) console.log(err);
+    res.statusCode = err ? 400 : 200;
+    res.send(err || results);
+  });
+});
 
 app.get('/api/searchListings/:searchQuery', (req, res) => {
-  console.log('search listings get request received', req.params)
+  console.log(new Date())
   const { searchQuery } = req.params;
   model.getSearchResults(searchQuery, (err, results) => {
     if (err) console.log(err);
@@ -30,33 +31,40 @@ app.get('/api/searchListings/:searchQuery', (req, res) => {
   });
 });
 
-// app.post('/api/searchRecords', (req, res) => {
-//   const { searchQuery } = req.body;
-//   res.header('Access-Control-Allow-Origin', '*');
-//   model.postSearchRecord(searchQuery, (err, results) => {
-//     if (err) console.log(err);
-//     res.statusCode = err ? 400 : 200;
-//     res.send(err || results);
-//   });
-// });
+app.post('/api/searchRecords', (req, res) => {
+  console.log('post received', req.body);
+  const { searchQuery } = req.body;
+  res.header('Access-Control-Allow-Origin', '*');
+  model.postSearchRecord(searchQuery, (err, results) => {
+    if (err) console.log(err);
+    res.statusCode = err ? 400 : 200;
+    res.send(err || results);
+  });
+});
 
-// app.put('/api/searchListings/:listingId', (req, res) => {
-//   model.updateSearchListing(req.params.listingId, req.body, (err, results) => {
-//     if (err) console.log(err);
-//     res.statusCode = err ? 400 : 200;
-//     res.send(err || results);
-//   });
-// });
+app.put('/api/searchListings/:listingId', (req, res) => {
+  model.updateSearchListing(req.params.listingId, req.body, (err, results) => {
+    if (err) console.log(err);
+    res.statusCode = err ? 400 : 200;
+    res.send(err || results);
+  });
+});
 
-// app.delete('/api/searchListings/:listingId', (req, res) => {
-//   model.deleteSearchListing(req.params.listingId, (err, results) => {
-//     if (err) console.log(err);
-//     res.statusCode = err ? 400 : 200;
-//     res.send(err || results);
-//   });
-// });
+app.delete('/api/searchListings/:listingId', (req, res) => {
+  model.deleteSearchListing(req.params.listingId, (err, results) => {
+    if (err) console.log(err);
+    res.statusCode = err ? 400 : 200;
+    res.send(err || results);
+  });
+});
 
-app.use('/*', express.static(`${__dirname}/../public`));
+app.use('/', express.static(path.join(__dirname, '../public/')));
+app.use('/search/:query', express.static(path.join(__dirname, '../public/')));
+app.use('/listing:listingId', express.static(path.join(__dirname, '../public/')));
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.resolve(`${__dirname}/../public/index.html`));
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
