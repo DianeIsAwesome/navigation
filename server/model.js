bc5d27665c870630643fd227dd01013faecb3d49
Chris W <christopherkwildenradt@gmail.com>
2018-08-11 08:50:00 -0700

    Update all routes to work with postgres

--- server/model.js
+++ server/model.js
@@ -10,61 +10,86 @@ const config = {
 }
 
 const pool = new pg.Pool(config);
-let myClient;
 
 const getSearchResults = function (searchTerm, cb) {
     pool.connect((err, client) => {
-      myClient = client;
-      const query = format("SELECT * FROM searchListing WHERE listingId = " + searchTerm + ";")
-        myClient.query(query, (err, result) => {
+      const query = "SELECT * FROM searchlisting WHERE city like '%" + searchTerm + "%' limit 20;";
+        client.query(query, (err, result) => {
           if (err) {
-            console.log(query, err)
+            console.log(err, result)
           } else {
+            console.log(new Date())
             cb(null, result.rows)
           }
-        })
-    })
+        });
+    });
+};
+
+// "SELECT * FROM searchlisting WHERE city::text like '%san%' limit 10;"
+// "SELECT * FROM searchlisting WHERE city like '%" + searchTerm + "%' limit 1000;";
+
+const getSearchRecords = function (cb) {
+  pool.connect((err, client) => {
+    const query = format("SELECT * FROM searchRecord WHERE searchId < 10;")
+      client.query(query, (err, result) => {
+        if (err) {
+          console.log(query, err)
+        } else {
+          console.log(result.rows)
+          cb(null, result.rows)
+        }
+      });
+  });
+};
+
+const postSearchRecord = function (searchQuery, cb) {
+  pool.connect((err, client) => {
+    const query = format("INSERT INTO searchlisting (title, host, city, photoURL) VALUES ('test', 'test1', 'test2', 'test3');")
+      client.query(query, (err, result) => {
+        if (err) {
+          console.log(query, err)
+          cb(err);
+        } else {
+          // console.log(result.rows)
+          cb(null, result.rows)
+        }
+      });
+  });
+};
+
+const updateSearchListing = (id, data, cb) => {
+  pool.connect((err, client) => {
+    const query = format("UPDATE searchlisting SET title = 'newtitle', host = 'chris', city = 'san francisco', photoURL = 'nah' WHERE listingId = 2;")
+      client.query(query, (err, result) => {
+        if (err) {
+          console.log(query, err)
+          cb(err);
+        } else {
+          cb(null, 'success')
+        }
+      });
+  });
 }
 
-
-// const getSearchRecords = function (callback) {
-//   models.SearchRecord.find({})
-//     .sort('-createdAt')
-//     .exec(callback);
-// };
-
-// const postSearchRecord = function (searchQuery, callback) {
-//   const searchRecord = new models.SearchRecord({ text: searchQuery, createdAt: new Date() });
-//   searchRecord
-//     .save()
-//     .then(results => callback(null, results))
-//     .catch(err => callback(err));
-// };
-
-// const updateSearchListing = (id, data, cb) => {
-//   models.SearchListing.findByIdAndUpdate(id, data, {new: true}, (err, result) => {
-//     if (err) console.log(err);
-//     return cb(null, result);
-//   })
-// }
-
-// const deleteSearchListing = (id, cb) => {
-//   models.SearchListing.findByIdAndRemove(id, (err, results) => {  
-//     if (err) console.log(err);
-
-//     const response = {
-//         message: "Successfully deleted",
-//         id: id
-//     };
-//     return cb(null, response)
-//   });
-// }
+const deleteSearchListing = (id, cb) => {
+  pool.connect((err, client) => {
+    const query = format("DELETE FROM searchlisting WHERE listingId = 3;")
+      client.query(query, (err, result) => {
+        if (err) {
+          console.log(query, err)
+          cb(err);
+        } else {
+          cb(null, 'Successfuly deleted')
+        }
+      });
+  });
+}
 
 
 module.exports = {
   getSearchResults,
-  // getSearchRecords,
-  // postSearchRecord,
-  // updateSearchListing,
-  // deleteSearchListing,
+  getSearchRecords,
+  postSearchRecord,
+  updateSearchListing,
+  deleteSearchListing,
 };
